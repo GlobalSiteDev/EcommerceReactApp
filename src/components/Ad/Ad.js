5326aa44c4db79c2b6d88ef5acbcddc3ffbc8d90
Bohdan <global.site.dev@gmail.com>
2018-09-20 13:35:40 +0300

    Add styled Ad component

--- src/components/Ad/Ad.js
+++ src/components/Ad/Ad.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import classes from './Ad.css';
+
 const Ad = () => {
-    const adUrl = `/ads/?r=${Math.floor(Math.random()*1000)}`
+    const adUrl = `http://localhost:3000/ads/?r=${Math.floor(Math.random()*1000)}`
     return (
-        <div className="classes.AdContainer">
-            <img  src={adUrl} style={classes.Image} />
+        <div className={classes.AdContainer}>
+            <img  src={adUrl} className={classes.Image} />
+            <p className={classes.Text}>Lorem ipsum</p>
         </div>
     )
 }
